9446bfd14cd3f71aa287eb616c74cf56555a46ee
yoonsoyoung <hummingsy@gmail.com>
2022-01-22 00:29:39 +0900

    Update 변수 : 변수와 문자열_220122

--- 기초/변수/main.py
+++ 기초/변수/main.py
@@ -8,5 +8,15 @@ print(PI)
 print(a)
 print(PI, a, str)
 
-a = b = c = d = e =100
+# 다중 할당 -> tuple type
+a = b = c = d = e =100, 200
+print(type(a))
 print(a, b, c, d, e)
+
+# 다중 할당 시 여러 개 값을 여러 개 변수에 각각 저장하는 코드를 한 줄로 구현
+a,b,c,d = 100, 3.14, 'k', 'korea'
+print(a,b,c,d)
+
+# 문자열, 따옴표, 홑따옴표
+print('나는 엄마에게 말했다. "더 이상 카레는 먹기 싫어요!" 라고')
+print('나는 엄마에게 말했다. "더 이상 \'카레\'는 먹기 싫어요!" 라고')
