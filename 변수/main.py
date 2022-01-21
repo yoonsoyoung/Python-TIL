cd23c40b3932000a8169b96b48df1ed88c9e75af
yoonsoyoung <hummingsy@gmail.com>
2022-01-22 00:08:14 +0900

    init

---
+++ 변수/main.py
@@ -0,0 +1,12 @@
+# 상수 선언 및 값 할당
+# 파이썬에서는 다른 언어에서 const 상수 문법이 없음. 변경이 가능함
+PI = 3.14
+a = 100
+str = "Hello world!"
+
+print(PI)
+print(a)
+print(PI, a, str)
+
+a = b = c = d = e =100
+print(a, b, c, d, e)
